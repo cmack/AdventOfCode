30e89addc7e69c6837ddd5f327853b4b20c9a004
Charlie McMackin <charliemac@gmail.com>
2017-12-02 17:21:44 -0600

    Refine day2 cljs solution

--- 2017/cljs/day2/day2.cljs
+++ 2017/cljs/day2/day2.cljs
@@ -7,27 +7,22 @@
 (def input (io/slurp "../../inputs/day2/day2.txt"))
 
 (defn- listify-input [input-string]
-   (-> input-string
-         (string/split "\n")
-         (->> (map #(string/split % "\t"))
-              (map (partial map int)))))
+  (-> input-string
+      (string/split "\n")
+      (->> (map #(string/split % "\t"))
+           (map (partial map int)))))
 
 (defn- max-min-distance [line]
   (- (apply max line)
      (apply min line)))
 
-(defn- divisible-combinations [sequence]
-  (for [x sequence
-        y sequence
-        :when (and (not= x y)
-                   (> x y)
-                   (zero? (rem x y)))]
-    (list x y)))
-
 (defn- evenly-divisible [line]
-  (->> (divisible-combinations line)
-       (map (partial apply /))
-       (first)))
+  (first (for [x line
+               y line
+               :when (and (not= x y)
+                          (> x y)
+                          (zero? (rem x y)))]
+           (/ x y))))
 
 (defn checksum [input line-checksum-fn]
   (reduce + (map line-checksum-fn (listify-input input))))
