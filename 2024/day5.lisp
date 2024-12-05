9be878732fdd37f811297d1628c50e10a1b64fd8
Charlie McMackin <charlie.mac@gmail.com>
2024-12-05 15:01:13 -0600

    Revisit 2024 day5 to verify aha moment from part 2
    Just proving to myself that part 1 could have been done if I had the aha
    moment earlier.

--- 2024/day5.lisp
+++ 2024/day5.lisp
@@ -29,34 +29,15 @@
                     (s:split-sequence "|" rule :test #'string=))
                   rules)))
 
-(defun assoc-rules (parsed-rules)
-   (s:assort parsed-rules :key #'first))
-
-(defun make-rules (rules)
-  (let ((rules-table (make-hash-table :test #'eq)))
-    (mapc (lambda (rule)
-            (setf (gethash (first rule) rules-table)
-                  (adjoin (second rule) (gethash (first rule) rules-table))))
-            rules)
-    rules-table))
-
 (defun make-orderings (orderings)
   (mapcar (s:curry #'mapcar #'parse-integer)
           (mapcar #'s:words orderings)))
 
+(defun order (ordering sorter-fn)
+  (sort (copy-seq ordering) sorter-fn))
+
 (defun ordered-p (ordering sorter-fn)
-  (equal ordering (sort (copy-seq ordering) sorter-fn)))
-
-(defun seen-p (page seen rule-table)
-  (let ((befores (gethash page rule-table)))
-    (intersection seen befores)))
-
-(defun sequential-p (ordering rules-table)
-  (equal ordering
-         (nreverse (reduce (lambda (seen page)
-                             (unless (seen-p page seen rules-table)
-                               (push page seen)))
-                           ordering :initial-value nil))))
+  (equal ordering (order ordering sorter-fn)))
 
 (defun middle-elt (list)
   (assert (oddp (length list)))
@@ -64,54 +45,33 @@
 
 (defun parsed-input (input)
   (multiple-value-bind (raw-rules raw-orderings) (split-rules input)
-    (let* ((rules (parse-rules raw-rules))
-           (rules-table (make-rules rules)))
-      (values rules-table
-              (make-orderings raw-orderings)))))
-
-(defun challenge-1 (input)
-  (multiple-value-bind (rules-table orderings) (parsed-input input)
-    (reduce #'+
-            (mapcar #'middle-elt
-                    (s:filter (lambda (x) (sequential-p x rules-table))
-                              orderings)))
-    ;; (count-if #'middle-elt
-    ;;           (s:filter (lambda (x) (ordered-p x rule-sorter)) orderings))
-
-    ))
+    (values (parse-rules raw-rules)
+            (make-orderings raw-orderings))))
 
 (defun toposorter (ordering rules)
+  ;; Only make the sorter for the rules in the ordering.
   (let ((narrowed-rules
           (remove-if-not (lambda (rule) (member rule ordering))
                          rules :key #'car)))
     (s:toposort narrowed-rules)))
 
-(defun order (ordering sorter-fn)
-  (sort (copy-seq ordering) sorter-fn))
+(defun challenge-1 (input)
+  (multiple-value-bind (rules orderings) (parsed-input input)
+    (reduce #'+
+            (mapcar #'middle-elt
+                    (s:filter (lambda (x)
+                                (ordered-p x (toposorter x rules)))
+                              orderings)))))
 
 (defun challenge-2 (input)
-  (multiple-value-bind (rules-table orderings) (parsed-input input)
-    (multiple-value-bind (correct incorrect)
-        (s:partition (lambda (x) (sequential-p x rules-table)) orderings)
-      (declare (ignore correct))
-      (let ((rules (parse-rules (split-rules input))))
-        (reduce #'+
-                (mapcar #'middle-elt
-                        (mapcar (lambda (x)
-                                  (order x (toposorter x rules)))
-                                incorrect)))))))
-
-
-;; (defun challenge-1 (input)
-;;   (multiple-value-bind (raw-rules raw-orderings) (split-rules input)
-;;     (let* ((rules (parse-rules raw-rules))
-;;            ;; This would have been awesome if they rule graph wasn't inconsistent
-;;            (rule-sorter (s:toposort rules))
-;;            (orderings (make-orderings raw-orderings)))
-;;       (count-if #'middle-elt
-;;                 (s:filter (lambda (x) (ordered-p x rule-sorter)) orderings)))))
-
-
+  (multiple-value-bind (rules orderings) (parsed-input input)
+    (let* ((incorrect (remove-if (lambda (x) (ordered-p x (toposorter x rules)))
+                                 orderings)))
+      (reduce #'+
+              (mapcar #'middle-elt
+                      (mapcar (lambda (x)
+                                (order x (toposorter x rules)))
+                              incorrect))))))
 
 ;;; initial ideas: this feels like a priority queue problem. Try it out?  Next
 ;;; idea, enclose the rule as a lambda in a hash table, then apply all rules
