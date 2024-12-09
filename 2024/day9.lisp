d24de333efa91022eeab42fb193b11cb1ab3d9f8
Charlie McMackin <charlie.mac@gmail.com>
2024-12-09 15:19:31 -0600

    Add 2024 day 9 part 2
    Started hoping I could use serapeum:runs to get the chunks and write them into
    the right place in a functional manner...not happy I fell into needing indices
    to splice the chunks in with REPLACE. As I type I'm now thinking about
    reversing the second list and creating a "mask" of places to overwrite in the
    first and then merging them... sigh.

--- 2024/day9.lisp
+++ 2024/day9.lisp
@@ -13,10 +13,13 @@
           input))
 
 (defun test (input)
-  (parse-lines (with-input-from-string (in input)
-                 (loop for line = (read-line in nil)
-                       while line
-                       collect line))))
+  (first (parse-lines (with-input-from-string (in input)
+                        (loop for line = (read-line in nil)
+                              while line
+                              collect line)))))
+
+(defun day9 ()
+  (first (parse-lines (uiop:read-file-lines "day9-input.txt"))))
 
 (defun expand (input)
   (let ((expansion
@@ -29,22 +32,78 @@
 
 (defun compact (input)
   (loop for i below (length input)
-        for f = (elt input i)
+        for f = (svref input i)
         for end-file = (position-if #'numberp input :from-end t)
         until (= i end-file)
         when (null f)
-          do (rotatef (elt input i) (elt input end-file))
+          do (rotatef (svref input i) (svref input end-file))
         finally (return input)))
 
 (defun checksum (compacted)
-  (let ((disk (delete nil compacted)))
+  (let ((disk (substitute 0 nil compacted)))
     (reduce #'+ (map 'vector #'* disk (s:iota (length disk))))))
 
-(defun day9 ()
-  (first (parse-lines (uiop:read-file-lines "day9-input.txt"))))
 
 (defparameter *test* "2333133121414131402")
 
 
 (defun part1 (input)
   (checksum (compact (expand input))))
+
+(defun empty-space-p (size)
+  (lambda (space)
+    (>= (count nil space) size)))
+
+(defun file-space-p ()
+  (lambda (space)
+    (some #'numberp space)))
+
+(defun empty-space-pos (n)
+  (lambda ()
+    (search)))
+
+(defun fit-file (seq file start)
+  (let* ((write-start (position nil (elt seq start)))
+         (write-end (+ write-start (length file))))
+    (s:splice-seqf (elt seq start) :new file
+                                   :start write-start
+                                   :end write-end))
+  seq)
+
+(defun collapse-vector (seq)
+  (reduce (s:curry #'concatenate 'vector) seq))
+
+(defun nullify-vector (seq)
+  (coerce (make-list (length seq)) 'vector))
+
+;;; New thought:
+;;; have another expanded. delete nil reverse and get runs
+;;; iterate over main and s:splice in
+
+;;; expanded has all the nils; chunked does not and is reversed
+
+(defun compact2 (expanded)
+  ;; let* ((expanded (s:runs expanded-input)))
+  (loop for chunk-end = (position-if #'numberp expanded :from-end t)
+          then (position-if #'numberp expanded :from-end t :end (1+ chunk-start))
+        for chunk-val = (elt expanded chunk-end)
+        for chunk-start = (position-if (lambda (x) (not (eql x chunk-val))) expanded
+                                       :from-end t :end chunk-end)
+        while (and chunk-start (> chunk-start 0))
+        do (let* ((chunk-size (- chunk-end chunk-start))
+                  (empty-fits (search (make-list chunk-size) expanded :end2 (1+ chunk-start))))
+             (when (and chunk-start empty-fits)
+               (incf chunk-start)
+               (incf chunk-end)
+               ;; (format t "chunk: ~a end: ~a val: ~a empty-fits: ~a~%~s~%"
+               ;;         chunk-start chunk-end chunk-val empty-fits expanded)
+               (replace expanded expanded :start1 empty-fits :end1 (+ empty-fits chunk-size)
+                                          :start2 chunk-start :end2 chunk-end)
+               (replace expanded (make-list chunk-size) :start1 chunk-start :end1 chunk-end)))
+        finally (return expanded)))
+
+(defun part2 (input)
+  (checksum (compact2 (expand input))))
+
+;;; 6227018846942 too high
+;;; 6227018762750
