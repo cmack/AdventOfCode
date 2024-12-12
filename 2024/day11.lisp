e961a1a8a4bd16387d39c8576b871ee22c4b996d
Charlie McMackin <charlie.mac@gmail.com>
2024-12-12 08:21:59 -0600

    Add 2024 day 11 part 2
    I really wanted there to be a way to memoize something done with
    prefix-sums (s:scan) and s:frequencies... but I couldn't get it without
    iterating over the key-values of a hash table in the end.

--- 2024/day11.lisp
+++ 2024/day11.lisp
@@ -33,7 +33,36 @@
 (defun blink (input)
   (mapcan #'replace-stone input))
 
-(defun part1 (input times)
+(defun blink-2 (stone-freqs)
+  (loop
+    with new-counts = (make-hash-table)
+    for stone being the hash-keys of stone-freqs
+      using (hash-value stone-count)
+    do (cond ((= 0 stone)
+              (incf (gethash 1 new-counts 0) stone-count))
+             ((even-digits-p stone)
+              (mapcar (lambda (split)
+                        (incf (gethash split new-counts 0) stone-count))
+                      (split-number stone)))
+             (t
+              (incf (gethash (* stone 2024) new-counts 0) stone-count)))
+    finally (return new-counts)))
+
+(defun blink-times (blinker-fn input times)
   (loop repeat times
-        for x = (blink input) then (blink x)
-        finally (return (length x))))
+        for x = (funcall blinker-fn input)
+          then (funcall blinker-fn x)
+        finally (return x)))
+
+(defun part1 (input times)
+  (length (blink-times #'blink2 input times)))
+
+(defun part2 (input times)
+  ;; start with a hash table of initial values
+  ;; each iteration creates a new "frequencies" table
+  (s:sum (s:hash-table-values
+          (blink-times #'blink2 (s:frequencies input) times))))
+
+;;; This challenge seems like a place for using prefix sums but the data that
+;;; explodes in size is eluding my reasoning for applying s:scan.
+;;; Maybe Fenwick trees? https://en.wikipedia.org/wiki/Fenwick_tree
