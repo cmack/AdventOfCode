7583408ce4de501869c12cfffb0016c747966096
Charlie McMackin <charlie.mac@gmail.com>
2024-12-18 02:03:11 -0600

    Add 2024 day 12 part 2
    This was a doozy for me.
    
    Because the regions that I generated in part one included interior matches --
    not just the perimeter -- I decided to count the corners. I needed an
    operation that could work on each face / pixel of my region tree. After much
    research I decided to try a modified 2x2 marching square that marches around a
    pixel before moving to the next one.
    
    It was interesting learning and research but frustrating that I couldn't get
    it with much of what I made in part 1.

--- 2024/day12.lisp
+++ 2024/day12.lisp
@@ -44,6 +44,19 @@ MIIIIIJJEE
 MIIISIJEEE
 MMMISSJEEE")
 
+(defparameter *e* "EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE")
+
+(defparameter *t2* "AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA")
+
 ;; Reading: feels like a convex hull type problem
 ;; or does this feel like a voronoi type problem?
 ;; or k-nearest neighbor?
@@ -71,7 +84,11 @@ MMMISSJEEE")
   (defmoves north #C(0 -1))
   (defmoves south #C(0 1))
   (defmoves west #C(-1 0))
-  (defmoves east #C(1 0)))
+  (defmoves east #C(1 0))
+  (defmoves northeast #C(1 -1))
+  (defmoves southeast #C(1 1))
+  (defmoves southwest #C(-1 1))
+  (defmoves northwest #C(-1 -1)))
 
 (defun face-neighbors (map face)
   (declare (optimize (debug 3)))
@@ -143,3 +160,73 @@ MMMISSJEEE")
 
 (defun part1 (input)
   (reduce #'+ (fence-prices input)))
+
+(defun square-neighbors (map face)
+  (mapcar (lambda (lookfn) (funcall lookfn map face))
+          (list #'look-west #'look-northwest #'look-north #'look-northeast
+                #'look-east #'look-southeast #'look-south #'look-southwest)))
+
+(defun 2x2-march (map face)
+  (destructuring-bind (w nw n ne e se s sw) (square-neighbors map face)
+    (let ((c (list (apply #'aref map (complex->index face))
+                   face)))
+      (list (list c w nw n)
+            (list e c n ne)
+            (list se s c e)
+            (list s sw w c)))))
+
+(defun score-corners (2x2 matcher)
+  (loop
+    for k from 0
+    for f in 2x2
+    when (funcall matcher f)
+      sum (expt 2 k) into j
+    finally (return (case j
+                      ;; my god is this fizzbuzz in the wild...
+                      ;; I'm being explicit anyway.
+                      ;; empty diagonals worth 2
+                      ((5 10) 2)
+                      ;; empty, full, vertical, or horizontal worth 0
+                      ((0 3 6 9 12 15) 0)
+                      ;; all others are 1
+                      (t 1)))))
+
+(defun score-region (map region letter)
+  ;; counts the corners which is the same as straight fence segments
+  (declare (optimize (debug 3)))
+  (let ((faces (remove-if #'characterp region)))
+    (loop with seen = (make-hash-table :test #'equal)
+          for coord in faces
+          do (loop
+               for square in (2x2-march map coord)
+               unless (gethash square seen)
+                 do (setf (gethash square seen)
+                          (score-corners square (lambda (f)
+                                                  (and f
+                                                       (char= (first f) letter)
+                                                       (member (second f) faces))))))
+          finally (return (s:sum (s:hash-table-values seen))))))
+
+(defun straight-fence-prices (map)
+  (s:maphash-return (lambda (letter regions)
+                      (loop for (region areas perimeters) in regions
+                            sum (fence-price areas (score-region map region letter))))
+                    (faces map)))
+
+(defun part2 (input)
+  (s:sum (straight-fence-prices input)))
+
+
+;;; Hmm for part 2 could I maximize the count of Xs and Ys in a given region?
+;; if I had kept my perimeter faces this could have been easier.  Ugh. I think
+;; I could have used the rectangle principle for part 1 and it would've been
+;; so much quicker
+;; Hindsight: nope, this thought was all wrong
+
+
+;; For part2 -- counting spots / faces between corners. Starting to feel like raster ops
+;; on the grid directly would be smarter?
+
+
+;;; 867130 is too high
+;;; 865662 winner
