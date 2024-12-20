7ed2da6a38cfe558c3bc6b2545312161d55ba2b1
Charlie McMackin <charlie.mac@gmail.com>
2024-12-20 01:21:16 -0600

    Add 2024 day 14 part 2
    a lot of trial and error...

--- 2024/day14.lisp
+++ 2024/day14.lisp
@@ -73,7 +73,6 @@ p=9,5 v=-3,-3")
             (mod (imagpart translated) grid-y)))))
 
 (defun part1 (input grid-dims)
-  ;; Do I need a grid?
   (let ((robots (roboticize input)))
     (safety-factor (mapcar (lambda (robot)
                              (destructuring-bind (pos vel) robot
@@ -82,3 +81,26 @@ p=9,5 v=-3,-3")
                    grid-dims)))
 
 ;;; (part1 (day14) '(101 103))
+
+(defun part2 (input grid-dims)
+  (declare (optimize (debug 3)))
+  (loop
+    with robots = (roboticize input)
+    for time from 1 to 10000
+    for positions = (mapcar (lambda (robot)
+                              (destructuring-bind (pos vel) robot
+                                (translate-robot pos vel time grid-dims)))
+                            robots)
+    for grid = (make-array grid-dims :initial-element ".")
+    when (and (> (length (remove-duplicates positions :test #'equal))
+                 495)
+              (> (count-if (lambda (pos)
+                             (destructuring-bind (x y) pos
+                               (and (< 25 x 80)
+                                    (< 25 y 80))))
+                           positions)
+                 250))
+      do (loop for (x y) in positions
+               do (setf (aref grid x y) "#")
+               finally (format t "~%~%~%--------- ~d ---------~%~a~%"
+                               time grid))))
